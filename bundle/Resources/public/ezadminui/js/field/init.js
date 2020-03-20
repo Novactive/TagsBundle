f5befeca4d52564d383676c3a7fb59bccb580ac2
Florian ALEXANDRE <f.alexandre@novactive.com>
2020-03-20 11:23:23 +0100

    feat: js validation

--- bundle/Resources/public/ezadminui/js/field/init.js
+++ bundle/Resources/public/ezadminui/js/field/init.js
@@ -30,3 +30,50 @@
   $('.parent-selector-tree').find('.tags-modal-tree').tagsTree({'modal': true});
 
 })();
+
+(function (global) {
+    const SELECTOR_FIELD = '.ez-field-edit--eztags';
+
+    class EzTagsValidator extends global.eZ.BaseFieldValidator {
+        /**
+         * Validates the input
+         *
+         * @method validateInput
+         * @param {Event} event
+         * @returns {Object}
+         * @memberof EzStringValidator
+         */
+        validateInput(event) {
+            const isRequired = event.target.closest(SELECTOR_FIELD).querySelector('.tagssuggest').dataset.required === "1";
+            const isEmpty = !event.target.value;
+            const isError = (isEmpty && isRequired);
+            const label = event.target.closest(SELECTOR_FIELD).querySelector('.ez-field-edit__label').innerHTML;
+            const result = {isError};
+
+            if (isEmpty) {
+                result.errorMessage = global.eZ.errors.emptyField.replace('{fieldName}', label);
+            }
+            return result;
+        }
+    };
+
+    const validator = new EzTagsValidator({
+        classInvalid: 'is-invalid',
+        fieldSelector: SELECTOR_FIELD,
+        eventsMap: [
+            {
+                selector: '.ez-field-edit--eztags input.tagids',
+                eventName: 'change',
+                callback: 'validateInput',
+                errorNodeSelectors: ['.ez-field-edit__label-wrapper'],
+                invalidStateSelectors: ['input.tagids']
+            },
+        ],
+    });
+
+    validator.init();
+
+    global.eZ.fieldTypeValidators = global.eZ.fieldTypeValidators ?
+        [...global.eZ.fieldTypeValidators, validator] :
+        [validator];
+})(window);
